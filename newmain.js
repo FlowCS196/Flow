03ce39c9d53b6024f5eb03ed7f04b0e6d975afa0
mosemizrahi <32136588+mosemizrahi@users.noreply.github.com>
2017-10-15 13:56:30 -0500

    Mose

---
+++ newmain.js
@@ -0,0 +1,70 @@
+funcs = [];
+/*funcs.push(["i", "n", 1]);
+funcs.push(["s", "ans = 1;", 2]);
+funcs.push(["c", "n > 1", 3, 6]);
+funcs.push(["s", "ans *= n;", 4]);
+funcs.push(["s", "n -= 1", 5]);
+funcs.push(["s", "printer(ans);", 2]);
+funcs.push(["s", "printer(ans);", 7]);
+funcs.push(["e"]);*/
+
+funcs.push(["i", "arr", 1]);
+funcs.push(["s", "i = 0;", 2]);
+funcs.push(["c", "i < arr.length - 1", 3, 11]);
+funcs.push(["s", "j = 0;", 4]);
+funcs.push(["c", "j < arr.length - 1 - i", 5, 10]);
+funcs.push(["c", "arr[j] > arr[j+1]", 6, 9]);
+funcs.push(["s", "temp = arr[j];", 7]);
+funcs.push(["s", "arr[j] = arr[j+1];", 8]);
+funcs.push(["s", "arr[j+1] = temp;", 9]);
+funcs.push(["s", "j+=1;", 4]);
+funcs.push(["s", "i+=1;", 2]);
+funcs.push(["s", "printer(arr)", 12]);
+funcs.push(["e"]);
+
+/*funcs.push(["s", "num = 0", 1]);
+funcs.push(["i", "mod", 2])
+funcs.push(["c", "num % mod == 0", 3, 5])
+funcs.push(["s", "cleaner()", 4]);
+funcs.push(["s", "printer(num); sleeper(1000)", 5]);
+funcs.push(["s", "++num", 2]);*/
+
+
+
+var box = document.getElementById('textbox');
+var worker = null;
+var runner = document.getElementById('runButton');
+var stopper = document.getElementById('stopButton');
+runner.onclick = function() {
+    if (worker != null) {
+        return;
+    }
+    worker = new Worker("newworker.js");
+    worker.onmessage = function(event) {
+        var pos;
+        if (event.data[0] == 'p') {
+            box.value += "" + event.data[1] + '\n';
+        } else if (event.data[0] == 'c') {
+            box.value = "";
+        } else if (event.data[0] == 'i') {
+            worker.postMessage(["i", event.data[1], event.data[2] + "=" + prompt("Enter Input", "0") + ";"]);
+        } else if (event.data[0] == 'e') {
+            if (event.data[1].length > 1) {
+                box.value += event.data[1] + '\n';
+            }
+            worker.terminate();
+            worker = null;
+        }
+    };
+    box.value = "";
+    worker.postMessage(funcs);
+}
+
+stopper.onclick = function() {
+    if (worker == null)
+        return;
+    else {
+        worker.terminate();
+        worker = null;
+    }
+}
---
+++ newworker.js
@@ -0,0 +1,77 @@
+const closer = (word) => {
+	this.postMessage(["e", word])
+}
+
+const printer = (word) => {
+	this.postMessage(["p", word]);
+}
+
+const cleaner = () => {
+	this.postMessage(["c"]);
+}
+const inputter = (word, next) => {
+	this.postMessage(["i", next.toString(), word]);
+}
+
+const sleeper = (milliseconds) => {
+	// Right now, this function does busy waiting. TODO: Implement a better solution.
+	var start = new Date().getTime();
+	while ((new Date().getTime() - start) < milliseconds) {}
+}
+
+const inputEvaluator = (word) => {
+	eval(word);
+}
+
+const complexEvaluator = (words) => {
+	if (words[0] == 's') {
+		eval(words[1]);
+		return words[2];
+	} else if (words[0] == 'c') {
+		return (eval(words[1])) ? words[2] : words[3];
+	} else if (words[0] == 'i') {
+		inputter(words[1], words[2]);
+		return -1;
+	} else if (words[0] == 'e') {
+		closer("");
+		return -1;
+	}
+}
+
+const run = (next, code) => {
+	inputEvaluator(code);
+	while (next >= 0) {
+		//printer(next)
+		next = complexEvaluator($functions[next]);
+	}
+}
+const creator = (rawFunctions) => {
+	for (let i = 0; i < rawFunctions.length; ++i) {
+		$functions.push(rawFunctions[i]);
+	}
+
+}
+
+const $functions = [];
+
+/*function functionize(phrase) {
+	if (phrase[0] == "s") {
+		return new Function(phrase[1] + "return " + phrase[2] + ";");
+	} else if (phrase[0] == 'c') {
+		return new Function("if (" + phrase[1] + ") return" + phrase[2] + "; return" + phrase[3] +";");
+	} else if (phrase[0] == 'i') {
+		return new Function("inputter(\"" + phrase[1] + "\"," + phrase[2] + "); return -1;");
+	} else if (phrase[0] == "e") {
+		return new Function("closer(\"\"); return -1;");
+	}
+}*/
+
+this.onmessage = function (event) {
+	if (event.data[0] === 'i') {
+		run (event.data[1], event.data[2]);
+	} else {
+		creator(event.data);
+		run(0, "");
+	}
+};
+
