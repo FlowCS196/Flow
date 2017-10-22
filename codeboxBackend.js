e5796d429ac2e84b38a384f864d61e0d56f8f4e5
mosemizrahi <32136588+mosemizrahi@users.noreply.github.com>
2017-10-21 22:30:43 -0500

    Functions to create/delete diagram boxes and make/remove connections between boxes, and a function to make code out of the boxes.

---
+++ codeboxBackend.js
@@ -0,0 +1,132 @@
+var codeBoxArr = [];
+
+/**
+ * Creates a div that has a textarea as a child. These will be the diagram boxes that you can drag around and fill with code.
+ * @param {string} type is the type of the box. 
+ *                      'f' means 'first', or the starting block. It leads to its primary connection box.
+ *                      's' means a simple box that leads to its primary connection box.
+ *                      'c' means 'conditional'. It leads to its primary connection box if its condition it true. Else, it leads to its secondary connection box.
+ *                      'e' means 'end'. The program halts when these boxes are reached. We do not actually need these right now.
+ */
+function addBox(type) {
+    function actualAdd(index) {
+        var codeBox = [document.createElement("div"), document.createElement("textarea"), type, undefined, undefined, []];
+
+        codeBox[0].id = "" + codeBoxArr.length;
+        codeBox[0].className = type + "_box";
+
+        codeBox[1].className = type + "_code";
+        codeBox[1].value = "";
+
+        codeBox[0].appendChild(codeBox[1]);
+
+        document.body.appendChild(codeBox[0]);
+        codeBoxArr.push(codeBox);
+    }
+    for (let i = 0; i < codeBoxArr.length; ++i) {
+        if (codeBoxArr[i] == null) {
+            actualAdd(i);
+            return;
+        }
+    }
+    actualAdd(codeBoxArr.length);
+}
+ 
+//index is the index of the box you are removing
+function removeBox(index) {
+    document.body.removeChild(codeBoxArr[index][0]);
+    codeBoxArr[index] = null;
+}
+
+
+//You are setting a primary connection from the box with ID startIndex to the box with ID endIndex.
+function primaryConnect(startIndex, endIndex) {
+    let start = parseInt(startIndex);
+    let end = parseInt(endIndex);
+    codeBoxArr[start][3] = parseInt(end);
+    codeBoxArr[end][5].push(start);
+}
+
+function secondaryConnect(startIndex, endIndex) {
+    let start = parseInt(startIndex);
+    let end = parseInt(endIndex);
+    codeBoxArr[start][4] = parseInt(end);
+    codeBoxArr[end][5].push(start);
+}
+
+//You are removing the primary connection from the box with ID startIndex. The endIndex is already known.
+function primaryUnconnectStart(startIndex) {
+    let start = parseInt(startIndex);
+    let end = codeBoxArr[start][3];
+    if (end == undefined) {
+        return;
+    }
+    codeBoxArr[start][3] = undefined;
+    codeBoxArr[end][5] = codeBoxArr[end][5].filter(num => num != start);
+}
+
+function secondaryUnconnectStart(startIndex) {
+    let start = parseInt(startIndex);
+    let end = codeBoxArr[start][4];
+    if (end == undefined) {
+        return;
+    }
+    codeBoxArr[start][4] = undefined;
+    codeBoxArr[end][5] = codeBoxArr[end][5].filter(num => num != start);
+}
+
+//You are removing all the connections that lead to the box with ID endIndex. The list of boxes that lead to it is already known.
+function unconnectEnd(endIndex) {
+    let end = parseInt(endIndex);
+    for (let i = 0; i < codeBoxArr[end][5].length; ++i) {
+        primaryUnconnectStart("" + codeBoxArr[end][5][i]);
+    }
+    codeBoxArr[end][5] = [];
+}
+
+
+
+
+function codeMaker() {
+    code = [];
+    var start;
+    for (let i = 0; i < codeBoxArr.length; ++i) {
+        if (codeBoxArr[i][2] == 'f') {
+            start = i;
+            let tmp = codeBoxArr[i];
+            code.push(['s', "", tmp[3]]);
+            break;
+        }
+    }
+    for (let i = 0; i < start; ++i) {
+        let tmp = codeBoxArr[i];
+        let line = [tmp[2], tmp[1].value];
+        if (tmp[3] != undefined) {
+            line.push(tmp[3]);
+            if (tmp[4] != undefined) {
+                line.push(tmp[4]);
+            }
+        } else {
+            line = ['e'];
+        }
+        code.push(line);
+        code.push(line);
+    }
+    for (let i = start + 1; i < codeBoxArr.length; ++i) {
+        let tmp = codeBoxArr[i];
+        let line = [tmp[2], tmp[1].value];
+        if (tmp[3] != undefined) {
+            line.push(tmp[3]);
+            if (tmp[4] != undefined) {
+                line.push(tmp[4]);
+            }
+        } else {
+            line = ['e'];
+        }
+        code.push(line);
+    }
+    return code;
+}
+
+
+
--- newmain.js
+++ newmain.js
@@ -1,4 +1,3 @@
-funcs = [];
 /*funcs.push(["i", "n", 1]);
 funcs.push(["s", "ans = 1;", 2]);
 funcs.push(["c", "n > 1", 3, 6]);
@@ -8,7 +7,7 @@ funcs.push(["s", "printer(ans);", 2]);
 funcs.push(["s", "printer(ans);", 7]);
 funcs.push(["e"]);*/
 
-funcs.push(["i", "arr", 1]);
+/*funcs.push(["i", "arr", 1]);
 funcs.push(["s", "i = 0;", 2]);
 funcs.push(["c", "i < arr.length - 1", 3, 11]);
 funcs.push(["s", "j = 0;", 4]);
@@ -20,7 +19,7 @@ funcs.push(["s", "arr[j+1] = temp;", 9]);
 funcs.push(["s", "j+=1;", 4]);
 funcs.push(["s", "i+=1;", 2]);
 funcs.push(["s", "printer(arr)", 12]);
-funcs.push(["e"]);
+funcs.push(["e"]);*/
 
 /*funcs.push(["s", "num = 0", 1]);
 funcs.push(["i", "mod", 2])
@@ -29,13 +28,49 @@ funcs.push(["s", "cleaner()", 4]);
 funcs.push(["s", "printer(num); sleeper(1000)", 5]);
 funcs.push(["s", "++num", 2]);*/
 
+/*addBox('f');
+addBox('s')
+addBox('i');
+addBox('c');
+addBox('s');
+addBox('s');
+addBox('s');
 
+primaryConnect('0','1');
+primaryConnect('1','2');
+primaryConnect('2','3');
+primaryConnect('3','4');
+secondaryConnect('3','6');
+primaryConnect('4','5');
+primaryConnect('5','6');
+primaryConnect('6','3');
+secondaryUnconnectStart('3');
+secondaryConnect('3', '6');
+unconnectEnd('5');
+primaryConnect('4', '5');*/
+
+addBox('f');
+addBox('s');
+addBox('s');
+addBox('c');
+addBox('s');
+addBox('s');
+
+primaryConnect('0','1');
+primaryConnect('1','2');
+primaryConnect('2', '3');
+primaryConnect('3','4');
+secondaryConnect('3', '2');
+primaryConnect('4','5');
 
 var box = document.getElementById('textbox');
 var worker = null;
 var runner = document.getElementById('runButton');
 var stopper = document.getElementById('stopButton');
 runner.onclick = function() {
+    var funcs = codeMaker();
+    //box.value = JSON.stringify(funcs);
+    box.value = "";
     if (worker != null) {
         return;
     }
@@ -47,7 +82,7 @@ runner.onclick = function() {
         } else if (event.data[0] == 'c') {
             box.value = "";
         } else if (event.data[0] == 'i') {
-            worker.postMessage(["i", event.data[1], event.data[2] + "=" + prompt("Enter Input", "0") + ";"]);
+            worker.postMessage(['i', event.data[1], event.data[2] + "=" + prompt("Enter Input", "0") + ";"]);
         } else if (event.data[0] == 'e') {
             if (event.data[1].length > 1) {
                 box.value += event.data[1] + '\n';
@@ -56,7 +91,6 @@ runner.onclick = function() {
             worker = null;
         }
     };
-    box.value = "";
     worker.postMessage(funcs);
 }
 
