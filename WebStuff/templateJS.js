7254a4c78c24aa765f1f690a81b7ff5a93b5ff3b
Julia Son <juliass2@illinois.edu>
2017-11-05 01:36:51 -0500

    changes draggable to have border

--- WebStuff/templateJS.js
+++ WebStuff/templateJS.js
@@ -1,3 +1,15 @@
-document.getElementById("simple").onclick = function() {
+document.getElementById("s").onclick = function() {
 	addBox("s");
+}
+document.getElementById("c").onclick = function() {
+	addBox("c");
+}
+document.getElementById("f").onclick = function() {
+	addBox("f");
+}
+document.getElementyById("i").onclick = function() {
+	addBox("i");
+}
+document.getElementyById("e").onclick = function() {
+	addBox("e");
 }
