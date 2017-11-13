5ad4d84c433926732b3102b9b303cfa127ff2fa4
mosemizrahi <32136588+mosemizrahi@users.noreply.github.com>
2017-11-13 01:10:44 -0600

    Important bug fix: The program won't fail when a conditional tries to evaluate a path that doesn't exist.

--- newworker.js
+++ newworker.js
@@ -32,7 +32,16 @@ const complexEvaluator = (words) => {
 		eval(words[1]);
 		return words[2];
 	} else if (words[0] == 'c') {
-		return (eval(words[1])) ? words[2] : words[3];
+		let truth = eval(words[1]);
+		if (truth) {
+			return words[2];
+		} else {
+			if (words[3] !== undefined) {
+				return words[3];
+			}
+			closer("");
+			return -1;
+		}
 	} else if (words[0] == 'i') {
 		inputter(words[1], words[2]);
 		return -1;
